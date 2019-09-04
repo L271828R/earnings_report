85025b47a94af95c4d480ae8138ab2ac13dca53d
BuildTools <luminai@gmail.com>
2019-09-04 15:24:10 -0400

    lr init

---
+++ er.py
@@ -0,0 +1,39 @@
+from pymongo import MongoClient
+import pymongo
+import numpy as np
+import datetime
+from tickers import tickers
+
+
+def report(arr):
+    _arr = sorted(arr, key = lambda x: x['date'])
+    for r in _arr:
+        earnings_date = str(r['date'])
+        market_cap = str(r['market-cap'])
+        ans = "{:<8}{:<22}{:<11}{}".format(
+            r['ticker'], 
+            earnings_date, 
+            market_cap, 
+            r['sector'])
+        print(ans)
+
+
+if __name__ == '__main__':
+    print('hello earnings report')
+    time_stamps = []
+    conn = MongoClient()
+    db = conn.database
+    collection = db.basic_data
+    arr = []
+    for ticker in tickers:
+        r = collection.find({'ticker': ticker}).sort([('earnings-date-iso', -1)]).limit(0)[0]
+        if not r['earnings-date-iso'] in [None, '']:
+            arr.append({
+                'ticker': r['ticker'], 
+                'date':r['earnings-date-iso'], 
+                'sector': r['sector'],
+                'market-cap': r['market-cap'],
+                })
+    report(arr)
+
+
---
+++ tickers.py
@@ -0,0 +1,66 @@
+# tickers=['TWLO']
+
+
+tickers=['spy',
+'mo',
+'aapl',
+'huya',
+'twlo',
+'tsla',
+'goog',
+'bidu',
+'kl', # Kirkland Gold
+'nvda',
+'bynd',
+'eght',
+'amd',
+'feye',
+'grub',
+'zen',
+'ge',
+'svxy',
+'uvxy', # has options tvix does not
+'spot',
+'jcp',
+'nflx',
+'irbt',
+'twtr',
+'gs',
+'msft',
+'amzn',
+'snap',
+'el',  # Estee Lauder Big Mover
+'shop',
+'uber',
+'qcom',
+'tgt', # 20% on last earnings
+'splk',
+'jnug',
+'dust',
+'ugaz',
+'dgaz',
+'mtch',
+'svmk', # survey monkey
+'see',  # sealed air corp sing.
+'ehth', # eHealth 
+'baba',
+'wmt',
+'amat',
+'soxl',
+'soxs',
+'tru',
+'fico', # FICO
+'symc', # Symantec
+'fvrr', # Fivr isreal.
+'goos', # canada goose
+'ayx',  # alterx data analysis
+'cldr', # cloudera anaconda spark hourton
+'zbra', # zebra big mover
+'yelp', # yelp
+'f',    # Ford
+'intu',
+'ostk',
+'cop',
+'roku',
+'lk',
+'vxx']

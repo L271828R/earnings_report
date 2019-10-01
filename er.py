8bf10735225cea593b72637c0d0fd38a301b8bd9
BuildTools <luminai@gmail.com>
2019-10-01 01:36:34 -0400

    lr date line and vol

--- er.py
+++ er.py
@@ -16,12 +16,36 @@ def report(arr):
             print("   --------------------------------")
             madeLine = True
         market_cap = str(r['market-cap'])
-        ans = "   {:<8}{:<22}{:<11}{}".format(
+        vds = ""
+        for vol in r['vol_data']:
+            vd = "{}={:<10} ".format(vol['d'], vol['be'])
+            vds = vds + vd
+
+        ans = "   {:<8}{:<22}{:<11}{:<30}{:<10}".format(
             r['ticker'], 
             earnings_date, 
             market_cap, 
-            r['sector'])
+            r['sector'],
+            vds 
+            )
         print(ans)
+
+def get_straddle_vols(conn, ticker):
+    collection = conn.database.straddles
+    dates = [ datetime.datetime.strptime(x, "%Y-%m-%d") for x in collection.distinct("time_stamp", {'ticker': ticker})]
+    arr = []
+    try:
+        max_date = datetime.datetime.strftime(max(dates), "%Y-%m-%d")
+        vols = collection.find({'ticker': ticker, 'time_stamp': max_date})
+        for item in vols:
+            if item['days_to_expiration'] < 30 and item['type'] == 'put-in-the-money':
+                arr.append({
+                    'd': item['days_to_expiration'],
+                    'be': item['be']
+                })
+    except:
+        return []
+    return arr
 
 
 if __name__ == '__main__':
@@ -37,11 +61,13 @@ if __name__ == '__main__':
             r['sector'] = 'None'
 
         if not r['earnings-date-iso'] in [None, '']:
+            vol_data = get_straddle_vols(conn, r['ticker'])
             arr.append({
                 'ticker': r['ticker'], 
                 'date':r['earnings-date-iso'], 
                 'sector': r['sector'],
                 'market-cap': r['market-cap'],
+                'vol_data': vol_data
                 })
     report(arr)
 
